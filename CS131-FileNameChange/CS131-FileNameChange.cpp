ec5a14acb6e23deb3a26a6c28b3c6ac34b66aa64
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-08-02 11:12:33 -0700

    Finsihed project

--- CS131-FileNameChange/CS131-FileNameChange.cpp
+++ CS131-FileNameChange/CS131-FileNameChange.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <fstream>
-
-// Include any necessary libraries here.
 
 using namespace std;
 
 int main()
 {
+	string inputFileName;
+	cin >> inputFileName;
+	ifstream inputFile(inputFileName);
+	string fileName;
 
-	/* Type your code here. */
+	while (getline(inputFile, fileName))
+	{
+		int position = static_cast<int>(fileName.find("_photo.jpg"));
+		fileName.replace(position, strlen("_photo.jpg"), "_info.txt");
+		cout << fileName << endl;
+	}
+
+	inputFile.close();
 
 	return 0;
 }
